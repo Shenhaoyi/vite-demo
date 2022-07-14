65bf8ab9fdfdfdf84caa598394e4919a8de66a6a
Shenhaoyi <shenhaoyi04@163.com>
2022-07-14 11:28:26 +0800

    移动index.html所在目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // index.html所在目录
+  root: path.join(process.cwd(), 'src'),
   plugins: [vue()]
 })
