3e5e776641b349dc18089af56aa126a5f7081762
Shenhaoyi <shenhaoyi04@163.com>
2022-07-15 17:09:20 +0800

    css modules 配置初探

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,11 @@ export default defineConfig({
         // additionalData 的内容会在每个 scss 文件的开头自动注入(用于全局变量使用，以及全局样式覆盖)
         additionalData: `@import "${path.resolve('./src/styles/mixins.less')}";`
       },
-    }
+    },
+    // CSS Module
+    modules: {
+      // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义（好像没有生效？）
+      generateScopedName: "[name]__[local]___[hash:base64:5]" // name 表示当前文件名，local 表示类名，5表示生成的哈希值长度
+    },
   }
 })
