056b406d052830865eaabcc1a2335976d5166715
Shenhaoyi <shenhaoyi04@163.com>
2022-07-15 11:58:08 +0800

    less全局样式/变量自动导入

---
+++ src/components/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="test"></div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="less" scoped>
+@import "../styles/mixins.less";
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
@@ -7,4 +7,22 @@ export default defineConfig({
   // index.html所在目录, 默认为process.cwd()
   // root: path.join(process.cwd(), 'src'),
   plugins: [vue()],
+  resolve: {
+    // 别名，@rollup/plugin-alias的入口
+    alias: [
+      {
+        find: /@\//,
+        replacement: path.join(process.cwd(), 'src'),
+      },
+    ],
+  },
+  css: {
+    // 预处理器
+    preprocessorOptions: {
+      less: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入(用于全局变量使用，以及全局样式覆盖)
+        additionalData: `@import "${path.resolve('./src/styles/mixins.less')}";`
+      },
+    }
+  }
 })
