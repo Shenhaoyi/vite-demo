e13a62417b568bc437f8d982c542ef458a14efe4
Shenhaoyi <shenhaoyi04@163.com>
2022-08-04 14:29:47 +0800

    feat:配置prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,39 @@
 module.exports = {
-  env: { // 环境设置
+  env: {
+    // 环境设置
     browser: true,
-    es2021: true
+    node: true, // 能够直接使用 module 这种cjs
+    es2021: true,
   },
   extends: [
     // 第1种情况
     'eslint:recommended',
     // 第2种情况，一般配置的时候可以省略 `eslint-config`
-    'standard',
+    // 'standard',
     // 第3种情况，可以省略包名中的 `eslint-plugin`
     // 格式一般为: `plugin:${pluginName}/${configName}`
     'plugin:vue/vue3-essential',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    // 接入prettier规则
+    'prettier',
+    'plugin:prettier/recommended',
   ],
+  parser: 'vue-eslint-parser', // 提供解析vue template的能力
+  // 优先级低于parse的语法解析配置
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser', // 提供解析ts的能力
-    sourceType: 'module' // 使用 ES Module
+    sourceType: 'module', // 使用 ES Module
   },
-  plugins: ['vue', '@typescript-eslint'], // 通过插件引入对应的规则，引入的规则默认都不开启
+  plugins: ['vue', '@typescript-eslint', 'prettier'], // 通过插件引入对应的规则，引入的规则默认都不开启
   rules: {
     // 单规则设置
+    'prettier/prettier': 'warn', // 注意要加上这一句，开启 prettier 自动修复的功能
+    '@typescript-eslint/no-explicit-any': 'error', // 禁止使用any
   },
   globals: {
     // 不可重写
     $: false,
-    jQuery: false
-  }
-}
+    jQuery: false,
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig, normalizePath } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
-import Unocss from 'unocss/vite'
+import { defineConfig, normalizePath } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
+import Unocss from 'unocss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // index.html所在目录, 默认为process.cwd()
   // root: path.join(process.cwd(), 'src'),
-  plugins: [
-    vue(),
-    Unocss(),
-  ],
+  plugins: [vue(), Unocss()],
   resolve: {
     // 别名，@rollup/plugin-alias的入口
     alias: [
@@ -25,13 +22,13 @@ export default defineConfig({
     preprocessorOptions: {
       less: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入(用于全局变量使用，以及全局样式覆盖)
-        additionalData: `@import "${path.resolve('./src/styles/mixins.less')}";`
+        additionalData: `@import "${path.resolve('./src/styles/mixins.less')}";`,
       },
     },
     // CSS Module
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义（好像没有生效？）
-      generateScopedName: "[name]__[local]___[hash:base64:5]" // name 表示当前文件名，local 表示类名，5表示生成的哈希值长度
+      generateScopedName: '[name]__[local]___[hash:base64:5]', // name 表示当前文件名，local 表示类名，5表示生成的哈希值长度
     },
-  }
-})
+  },
+});
