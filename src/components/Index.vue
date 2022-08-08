d00497eace60219d22c36f86ea7949c21424d396
Shenhaoyi <shenhaoyi04@163.com>
2022-08-08 14:13:12 +0800

    feat: 图片资源使用

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,12 +2,22 @@
   <div class="test test2" :class="{ mt5: true }"></div>
   <div class="test test2" ml10px :class="['mt10px']"></div>
   <div class="i-ep-search" shen></div>
+  <img :src="logo" />
+  <div class="test3"></div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import logo from '@/assets/logo.png';
+</script>
 
 <style lang="less" scoped>
 .test2 {
   .test2();
+}
+
+.test3 {
+  width: 400px;
+  height: 400px;
+  background: url('@/assets/logo.png') no-repeat;
 }
 </style>
