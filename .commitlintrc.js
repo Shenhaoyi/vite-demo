89f33941af723413d2925628fc1c4471366771b3
Shenhaoyi <shenhaoyi04@163.com>
2022-08-08 11:46:40 +0800

    feat: 自定义commitlint规则

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,12 @@
 // .commitlintrc.js
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['cz'],
+  rules: {
+    // <type> 不能为空
+    'type-empty': [2, 'never'],
+    // <type> 格式 小写
+    'type-case': [2, 'always', 'lower-case'],
+    // <subject> 不能为空
+    'subject-empty': [2, 'never'],
+  },
 };
